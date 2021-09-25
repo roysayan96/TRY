1ab406a007f90afdf717ef726b341c50b4d984df
shiny <shiny.majumder12@gmail.com>
2021-09-25 22:59:18 +0530

    want to add new line

---
+++ pattern - Copy.cpp
@@ -0,0 +1,20 @@
+#i just want to try git
+#jkcjdjfdojfi
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter the no of rows:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
